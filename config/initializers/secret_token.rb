3907a52d0d014069c5c59ebe79b4d9e97325ff59
kh2ouija <kh2ouija@yahoo.com>
2013-08-28 12:03:09 +0300

    new token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bankofdad::Application.config.secret_key_base = '63351221b156a7d435c3a8a00da579ae79e4db56546d40b2c308c84cd49c450ca4cebd021895abab192f4e25046f898c9c4e65fcb62ea14e7f7bbd0b956eaf94'
