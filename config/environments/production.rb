0a4efc4d1b041b42928b511156b3c5536cd4ab2a
kh2ouija <kh2ouija@yahoo.com>
2013-08-30 01:36:02 +0300

    fixed unit tests

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Bankofdad::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'bankofdad.eu' }
+  config.action_mailer.default_url_options = { :host => 'beta.bankofdad.eu' }
 end
