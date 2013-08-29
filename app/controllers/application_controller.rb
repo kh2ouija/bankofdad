1928b4f29f5f14b96c3b35aa5722399273922f73
kh2ouija <kh2ouija@yahoo.com>
2013-08-29 17:49:24 +0300

    time zone selection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :set_timezone
+
+  def set_timezone
+    tz = request.cookies['jstz_time_zone']
+    Time.zone = tz unless tz.nil?
+  end
 end
