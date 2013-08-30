353834eaecda40fecf054722c51c79835f6f9874
kh2ouija <kh2ouija@yahoo.com>
2013-08-30 21:23:36 +0300

    capistrano setup

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,14 +12,12 @@ role :web, "beta.bankofdad.eu"                          # Your HTTP server, Apac
 role :app, "beta.bankofdad.eu"                          # This may be the same as your `Web` server
 role :db,  "beta.bankofdad.eu", :primary => true # This is where Rails migrations will run
 
-set :user, "sp"
 set :use_sudo, false
-set :deploy_to, "/home/sp/bankofdad"
 
 namespace :deploy do
   task :copy_configs do
-    run "cp /home/sp/database.yml #{release_path}/config/"
-    run "cp /home/sp/newrelic.yml #{release_path}/config/"
+    run "cp #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "cp #{shared_path}/config/newrelic.yml #{release_path}/config/newrelic.yml"
   end
-  after "deploy:update_code", "deploy:copy_configs"
+  before "deploy:assets:precompile", "deploy:copy_configs"
 end
