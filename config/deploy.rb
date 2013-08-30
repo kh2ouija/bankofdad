02fff34e97c7d65b6d61d811ea8a54a9827b5a15
kh2ouija <kh2ouija@yahoo.com>
2013-08-31 00:33:00 +0300

    removed beta, added to landing page

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,9 +8,9 @@ set :repository_cache, "git_cache"
 set :deploy_via, :remote_cache
 set :ssh_options, { :forward_agent => true }
 
-role :web, "beta.bankofdad.eu"                          # Your HTTP server, Apache/etc
-role :app, "beta.bankofdad.eu"                          # This may be the same as your `Web` server
-role :db,  "beta.bankofdad.eu", :primary => true # This is where Rails migrations will run
+role :web, "bankofdad.eu"
+role :app, "bankofdad.eu"
+role :db,  "bankofdad.eu", :primary => true
 
 set :use_sudo, false
 
@@ -18,6 +18,7 @@ namespace :deploy do
   task :copy_configs do
     run "cp #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "cp #{shared_path}/config/newrelic.yml #{release_path}/config/newrelic.yml"
+    run "cp #{shared_path}/config/initializers/secret_token.rb #{release_path}/config/initializers/secret_token.rb"
   end
   before "deploy:assets:precompile", "deploy:copy_configs"
 end
