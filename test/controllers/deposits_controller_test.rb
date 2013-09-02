03a7bf1d367896b1cdbd043a5b383319e72c0b71
kh2ouija <kh2ouija@yahoo.com>
2013-09-03 00:43:54 +0300

    fixed tests

--- test/controllers/deposits_controller_test.rb
+++ test/controllers/deposits_controller_test.rb
@@ -2,48 +2,28 @@ require 'test_helper'
 
 class DepositsControllerTest < ActionController::TestCase
   setup do
+    sign_in :user, users(:one)
     @deposit = deposits(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:deposits)
-  end
-
   test "should get new" do
-    get :new
+    get :new, customer_id: @deposit.customer
     assert_response :success
   end
 
   test "should create deposit" do
     assert_difference('Deposit.count') do
-      post :create, deposit: { balance: @deposit.balance, customer_id: @deposit.customer_id, duration_months: @deposit.duration_months, interest: @deposit.interest }
+      post :create, customer_id: @deposit.customer, deposit: { balance: @deposit.balance, customer: @deposit.customer, duration_months: @deposit.duration_months, interest: @deposit.interest }
     end
 
-    assert_redirected_to deposit_path(assigns(:deposit))
-  end
-
-  test "should show deposit" do
-    get :show, id: @deposit
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @deposit
-    assert_response :success
-  end
-
-  test "should update deposit" do
-    patch :update, id: @deposit, deposit: { balance: @deposit.balance, customer_id: @deposit.customer_id, duration_months: @deposit.duration_months, interest: @deposit.interest }
-    assert_redirected_to deposit_path(assigns(:deposit))
+    assert_redirected_to @deposit.customer
   end
 
   test "should destroy deposit" do
     assert_difference('Deposit.count', -1) do
-      delete :destroy, id: @deposit
+      delete :destroy, customer_id: @deposit.customer, id: @deposit
     end
 
-    assert_redirected_to deposits_path
+    assert_redirected_to @deposit.customer
   end
 end
