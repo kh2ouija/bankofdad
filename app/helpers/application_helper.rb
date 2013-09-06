379e0f95d89e1497d4ce272241b67d59e04f355e
kh2ouija <kh2ouija@yahoo.com>
2013-09-07 00:54:55 +0300

    ui rework

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,9 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  def next_wday(n)
+    t = Date.today
+    t + (n - t.wday) % 7
+  end
 end
